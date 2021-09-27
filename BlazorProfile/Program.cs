ad567acee080d2412e97e836dde3ef72ee2c0149
MorningPants <77476821+MorningPants@users.noreply.github.com>
2021-09-27 13:15:43 -0700

    Update Git

--- BlazorProfile/Program.cs
+++ BlazorProfile/Program.cs
@@ -1,4 +1,5 @@
-using BlazorProfile;
+using BlazorProfile.Pages;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -8,5 +9,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
 await builder.Build().RunAsync();
